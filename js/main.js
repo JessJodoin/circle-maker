7742279da772a11c5eac801fa0496ea06c654334
Jessica Jodoin <jodo0083@algonquinlive.com>
2020-03-21 10:37:14 -0400

    Add content to the javascript file so it does the thing

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
 $('html').on('keydown', function (e) {
 
-  console.log(e.keyCode);
+  var $circle = $('<div>').addClass('ball');
+  var randLeft = Math.random() * 1500;
+  var randTop = Math.random() * 1500;
+
+  $('body').append($circle);
+  $circle.css('left', randLeft);
+  $circle.css('top', randTop);
 });
